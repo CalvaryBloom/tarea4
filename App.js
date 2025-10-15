0f78ab0500d6e2693619c97eff4e6b5b667c9079
CalvaryBloom <borjap99@gmail.com>
2025-10-15 08:56:10 +0200

    Ejercicio 4

--- App.js
+++ App.js
@@ -1,62 +1,30 @@
 /*
 *
-* Ejercicio 3:
-* A partir del código proporcionado, utiliza map, un cuadrado y un triángulo para renderizar una figura similar a la siguiente
+* Ejercicio 4:
+* A partir del código proporcionado, utiliza map y un círculo para renderizar una figura similar a la siguiente:
 */
 import { StyleSheet, View } from 'react-native';
 import { useState } from 'react';
 
 export default function App() {
   const [content, setContent] = useState([
-    {
-      widthSquareOne: 150,
-      colorSquareOne: 'blue',
-      widthSquareTwo: 50,
-      colorSquareTwo: 'blue',
-    },
-    {
-      widthSquareOne: 150,
-      colorSquareOne: 'red',
-      widthSquareTwo: 50,
-      colorSquareTwo: 'blue',
-    },
-    {
-      widthSquareOne: 150,
-      colorSquareOne: 'red',
-      widthSquareTwo: 100,
-      colorSquareTwo: 'blue',
-    },
-  ])
+    ['red', 'blue', 'green'],
+    ['blue', 'green', 'red'],
+    ['green', 'red', 'blue'],
+  ]);
+
   return (
     <View style={styles.container}>
-      <View style={{ flexDirection: 'row' }}>
-        <View
-          width={0}
-          height={0}
-          backgroundColor={'transparent'}
-          borderStyle={'solid'}
-          borderLeftWidth={50}
-          borderRightWidth={50}
-          borderBottomWidth={100}
-          borderLeftColor="transparent"
-          borderRightColor="transparent"
-          borderBottomColor="blue"
-        />
-      </View>
-      {content.map((value, index) => (
-        <View key={index.toString()} style={{ flexDirection: 'row' }}>
-          <View
-            width={value.widthSquareTwo}
-            height={value.widthSquareTwo}
-            backgroundColor={value.colorSquareTwo}
-          />
-          <View>
+      {[0, 1, 2].map((value, index) => (
+        <View style={{ flexDirection: 'row' }}>
+          {content[value].map((value, index) => (
             <View
-              width={value.widthSquareTwo}
-              height={value.widthSquareTwo}
-              backgroundColor={value.colorSquareTwo}
+              width={100}
+              height={100}
+              borderRadius={50}
+              backgroundColor={value}
             />
-          </View>
+          ))}
         </View>
       ))}
     </View>
